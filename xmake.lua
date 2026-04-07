f0c096623c6e103fcdad1cc2a9d7864bc8e3bab3
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2026-04-07 10:26:18 +0800

    chore：Update LeviLamina version to v26.10.0

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 
-add_requires("levilamina 1.9.0", {configs = {target_type = "server"}})
+add_requires("levilamina 26.10.0", {configs = {target_type = "server"}})
 
 add_requires("levibuildscript")
 add_requires("exprtk 0.0.3")
