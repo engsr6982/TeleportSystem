26d82190db107292c1d037dcfedb00318e6952a5
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2026-07-10 20:20:48 +0800

    适配 LeviLamina v26.20

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 
-add_requires("levilamina 26.10.0", {configs = {target_type = "server"}})
+add_requires("levilamina 26.20.0", {configs = {target_type = "server"}})
 
 add_requires("levibuildscript")
 add_requires("exprtk 0.0.3")
@@ -11,6 +11,9 @@ if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
+if is_plat("windows") then
+    set_toolchains("clang-cl") -- windows allways use clang-cl
+end
 
 option("test")
     set_default(false)
@@ -26,27 +29,31 @@ target("TeleportSystem")
     add_rules("gen_version")
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
-    add_cxflags(
-        "/EHa",
-        "/utf-8",
-        "/W4",
-        "/w44265",
-        "/w44289",
-        "/w44296",
-        "/w45263",
-        "/w44738",
-        "/w45204"
-    )
+    if is_plat("windows") then
+        add_defines("NOMINMAX", "UNICODE")
+        set_exceptions("cxx")
+        add_cxflags("/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+        add_cxflags(
+            "/EHs",
+            "-Wno-microsoft-cast",
+            "-Wno-invalid-offsetof",
+            "-Wno-c++2b-extensions",
+            "-Wno-microsoft-include",
+            "-Wno-overloaded-virtual",
+            "-Wno-ignored-qualifiers",
+            "-Wno-missing-field-initializers",
+            "-Wno-potentially-evaluated-expression",
+            "-Wno-pragma-system-header-outside-header",
+            {tools = {"clang_cl"}}
+        )
+    end
     set_kind("shared")
     set_symbols("debug")
     set_languages("c++20")
-    set_exceptions("none") -- To avoid conflicts with /EHa.
     add_includedirs("src")
     add_headerfiles("src/**.h")
     add_files("src/**.cc")
     add_defines(
-        "NOMINMAX",
-        "UNICODE",
         "_HAS_CXX23=1",
         "TPS_EXPORTS",
         "LL_PLAT_S"
