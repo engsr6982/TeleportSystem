56d626b5f715bdb35db190c271af1666ce01b84f
engsr6982 <pwxg286704@163.com>
2026-09-25 00:33:04 +0800

    feat: bump version to v0.20.0

--- xmake.lua
+++ xmake.lua
@@ -3,10 +3,10 @@ add_rules("mode.debug", "mode.release")
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 add_repositories("iceblcokmc https://github.com/IceBlcokMC/xmake-repo.git")
 
-add_requires("levilamina 26.40.4", {configs = {target_type = "server"}})
+add_requires("levilamina 26.51.5", {configs = {target_type = "server"}})
 
-add_requires("ll-bstats 0.6.0")
-add_requires("economy_bridge 0.6.0")
+add_requires("ll-bstats 0.7.0")
+add_requires("economy_bridge 0.7.0")
 
 add_requires("levibuildscript")
 add_requires("exprtk 0.0.3")
